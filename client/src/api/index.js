1522ef41a32637c5722b632f0f82800a336d8150
Tunahan Kuzucu <95137010+tnhnkzc@users.noreply.github.com>
2022-11-11 20:52:05 +0100

    Update index.js
    Change backend baseURL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://tunahankuzucu.herokuapp.com/" });
+const API = axios.create({ baseURL: "https://personal-portfolio-u11k.onrender.com/" });
 
 export const fetchProjects = () => API.get("/projects");
 
